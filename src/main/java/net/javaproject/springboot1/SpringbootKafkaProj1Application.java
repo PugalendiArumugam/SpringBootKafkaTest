0a603aafea1f24e6c04f72561d28fcd2945d5f53
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-07-18 23:23:50 +0530

    next version

---
+++ src/main/java/net/javaproject/springboot1/SpringbootKafkaProj1Application.java
@@ -0,0 +1,13 @@
+package net.javaproject.springboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaproject/springboot1/SpringbootKafkaProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaproject.springboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
